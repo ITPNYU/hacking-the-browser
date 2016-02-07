4cd62b46c204b854a2c4452297f88fd09bf15f03
Cory Forsyth <cory.forsyth@gmail.com>
2016-02-07 15:55:11 -0500

    page action example

---
+++ page-action-hello-world/background.js
@@ -0,0 +1,19 @@
+console.log('Background page version 2');
+
+// This listener is called when a tab becomes active
+// in a window (e.g. the user clicks on the tab icon)
+chrome.tabs.onActivated.addListener(function(details) {
+  var tabId = details.tabId;
+  var windowId = details.windowId;
+  console.log('tab activated with id:',tabId,
+              ' in window id: ', windowId);
+  chrome.pageAction.show(tabId);
+});
+
+// This is called when a tab is updated (e.g. when
+// it loads a new page or loads a favicon)
+chrome.tabs.onUpdated.addListener(function(tabId, change, tab) {
+  console.log('updated tab id: ',tabId,
+              ' to status: "',tab.status,
+              '" and url: url: ',tab.url);
+});
