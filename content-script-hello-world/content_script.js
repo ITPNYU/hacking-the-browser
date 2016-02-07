c25fd25fa08b9f5845a65ae8bfe81c1d39e3ccb6
Cory Forsyth <cory.forsyth@gmail.com>
2016-02-07 12:48:07 -0500

    hello world content script

---
+++ content-script-hello-world/content_script.js
@@ -0,0 +1,7 @@
+// The code in this file will load:
+//   * after the document is ready
+//   * after any previous content scripts (e.g., jquery.js)
+//
+// So you can safely use jQuery in the code below
+
+console.log("Hello, world! (from a content script) (version: 1)");
