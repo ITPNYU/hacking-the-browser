480df92295c55fcf3ab1da0ab81a02695380c95e
Cory Forsyth <cory.forsyth@gmail.com>
2016-02-07 14:39:50 -0500

    update content script js

--- content-script-hello-world/content_script.js
+++ content-script-hello-world/content_script.js
@@ -2,6 +2,9 @@
 //   * after the document is ready
 //   * after any previous content scripts (e.g., jquery.js)
 //
-// So you can safely use jQuery in the code below
+// So you can safely use jQuery (the `$`) in the code below
 
 console.log("Hello, world! (from a content script) (version: 1)");
+
+// change all paragraphs to red:
+// $('p').css({'color': 'red'});
