c2b35848da4dc0613a39c3eb7fa7f9450fbb80f4
Cory Forsyth <cory.forsyth@gmail.com>
2016-03-17 23:01:51 -0400

    Change stats-alyzer to be a page action

--- ncaa-stats-alyzer/background.js
+++ ncaa-stats-alyzer/background.js
@@ -17,3 +17,14 @@ chrome.runtime.onMessage.addListener(function(message, sender, respond) {
 
   return true;
 });
+
+// Add listener for when a tab is updated
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  console.log('updated',tabId,changeInfo,tab);
+  if (changeInfo.status === 'complete') {
+    if (tab.url.indexOf('sports-reference.com') !== -1) {
+      console.log('showing page action');
+      chrome.pageAction.show(tabId);
+    }
+  }
+});
