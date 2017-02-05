8b21c3acc1bc188e09856dff289056e5da2725d6
Cory <cory.forsyth@gmail.com>
2017-02-05 17:08:06 -0500

    update browser action hello world

--- animated-browser-action/background.js
+++ animated-browser-action/background.js
@@ -1,20 +1,22 @@
 console.log('animated browser action version 1');
 
 chrome.browserAction.onClicked.addListener(function() {
-
   // loop from 0 to 10, setting the icon to one of the images
   // in the images/ directory
-  for (var i = 0; i < 11; i++) {
-    setTimeout(function(i) {
-      return function() {
-        if (i === 10) {
-          // reset to upright (icon 0) at the end of the loop
-          i = 0;
-        }
-        // e.g. "images/rose-3.png"
-        var imagePath = 'images/rose-' + i + '.png';
+  for (let i = 0; i <= 10; i++) {
+    setTimeout(() => {
+      console.log('handling i:',i);
+      if (i === 10) {
+        resetIcon();
+      } else {
+        let imagePath = "images/rose-" + i + ".png";
         chrome.browserAction.setIcon({path: imagePath});
-      };
-    }(i), i*50);
+      }
+    }, i*50);
   }
 });
+
+// sets the icon back to upright (non-rotated) image
+function resetIcon() {
+  chrome.browserAction.setIcon({path: 'images/rose-0.png'});
+}
