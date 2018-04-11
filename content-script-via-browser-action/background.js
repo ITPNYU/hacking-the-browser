d065f162eb31694f0bb9b6c3f49489f96c0aa4c3
Cory <cory.forsyth@gmail.com>
2018-04-10 22:48:54 -0400

    update content-script-via-browser-action

--- content-script-via-browser-action/background.js
+++ content-script-via-browser-action/background.js
@@ -1,10 +1,24 @@
 console.log('Content Script via Browser Action background page, version 1');
-// Docs for 'onClicked' are here: https://developer.chrome.com/extensions/browserAction#event-onClicked
-chrome.browserAction.onClicked.addListener(function(tab) {
-  console.log('Clicked on tab: ', tab);
+// Docs for 'onClicked' are here:
+// https://developer.chrome.com/extensions/browserAction#event-onClicked
 
-  // Message can be a string or an object.
-  // See docs here: https://developer.chrome.com/extensions/tabs#method-sendMessage
-  var message = 'Go!';
-  chrome.tabs.sendMessage(tab.id, message);
+chrome.browserAction.onClicked.addListener(function() {
+  console.log('Clicked the browser action!');
+
+  // first, inject jquery:
+  chrome.tabs.executeScript({file: 'jquery.js'}, function() {
+    // jquery is injected, *now* inject our content script
+    console.log('background script injected jquery');
+
+    chrome.tabs.executeScript({file: 'content_script.js'}, function() {
+      console.log('background script injected content_script');
+    });
+  });
+
+  // If you want to inject a CSS file, uncomment the code below
+  // For more details on this method,
+  // see: https://developer.chrome.com/extensions/tabs#method-insertCSS
+  chrome.tabs.insertCSS({file: 'mystyles.css'}, function() {
+    console.log('background script inserted mystyles.css');
+  });
 });
--- content-script-via-browser-action/content_script.js
+++ content-script-via-browser-action/content_script.js
@@ -1,25 +1,7 @@
-// The code in this file will load:
-//   * after the document is ready
-//   * after any previous content scripts (e.g., jquery.js)
-//
-// So you can safely use jQuery (the `$`) anytime in the code below
+// This script needs jQuery present to run,
+// so the background script must inject jQuery *before* it
+// injects this one.
 
 console.log('Hello, world! (from a content script) (version: 1)');
 
-// Add a listener for a message from the background.js
-// "chrome.runtime" is one of the few chrome extension APIs available
-// to a content script
-// See docs here: https://developer.chrome.com/extensions/runtime#event-onMessage
-chrome.runtime.onMessage.addListener(function(message) {
-  console.log('Got message: ', message);
-
-  // The only message we expect is the "Go!" message, so this conditional
-  // isn't necessary, but you could use it to take different actions
-  // depending on what message was sent.
-  if (message === 'Go!') {
-    console.log('Making all paragraphs red');
-    // change all paragraphs to red:
-    $('p').css({'background-color': 'red'});
-  }
-});
-
+$('body').css({'background-color': 'red'});
