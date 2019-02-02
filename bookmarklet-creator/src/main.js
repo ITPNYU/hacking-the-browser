ae737065c6e77a6ad8b11357f65bc99a847e95d0
Cory <cory.forsyth@gmail.com>
2019-02-02 13:28:26 -0500

    editable bookmarklet name

--- bookmarklet-creator/src/main.js
+++ bookmarklet-creator/src/main.js
@@ -1,18 +1,22 @@
 class BookmarkletCreator {
-  constructor(textArea, { bookmarklet, bookmakletTextArea }) {
+  constructor(textArea, { bookmarklet, bookmakletTextArea, bookmarkletName }) {
     this.bookmarklet = bookmarklet;
     this.bookmakletTextArea = bookmakletTextArea;
+    this.bookmarkletName = bookmarkletName;
     this.editor = CodeMirror.fromTextArea(textArea, {
       theme: 'monokai',
       autofocus: true
     });
     this.update();
     this.editor.on('change', () => this.update());
+    this.bookmarkletName.addEventListener('input', () => this.update());
   }
 
   update() {
     let value = this.prepareValue(this.editor.getValue());
+    let name = this.bookmarkletName.value;
     this.bookmarklet.setAttribute('href', value);
+    this.bookmarklet.innerText = name;
     this.bookmakletTextArea.value = value;
   }
 
@@ -77,6 +81,7 @@ injectJquery(function($) {
 
 let bookmarklet = document.getElementById('bookmarklet');
 let bookmakletTextArea = document.getElementById('bookmarklet-text-area');
+let bookmarkletName = document.getElementById('bookmarklet-name');
 
 bookmakletTextArea.addEventListener('click', () => {
   bookmakletTextArea.select();
@@ -84,7 +89,8 @@ bookmakletTextArea.addEventListener('click', () => {
 
 let creator = new BookmarkletCreator(document.getElementById('editor'), {
   bookmarklet,
-  bookmakletTextArea
+  bookmakletTextArea,
+  bookmarkletName
 });
 
 addSnippets(creator);
